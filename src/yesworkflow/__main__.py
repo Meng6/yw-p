3020efd22cd8185ed884d4411e81f8918de556e1
Meng Li <34143965+Meng6@users.noreply.github.com>
2024-12-28 06:10:47 +0000

    Add the graph command

--- src/yesworkflow/__main__.py
+++ src/yesworkflow/__main__.py
@@ -3,10 +3,12 @@
 from yesworkflow.commands.cli import cli
 from yesworkflow.commands.extract import extract
 from yesworkflow.commands.model import model
+from yesworkflow.commands.graph import graph
 
 # Add commands
 cli.add_command(extract)
 cli.add_command(model)
+cli.add_command(graph)
 
 if __name__ == '__main__':
     cli()
--- src/yesworkflow/commands/graph.py
+++ src/yesworkflow/commands/graph.py
@@ -1,17 +1,21 @@
 from yesworkflow.parse.parse import create_model
 from yesworkflow.commands.extract import extract_annotations
-import click
+import click, geist
 
-@click.command(help='Builds workflow model from identified YW comments') # Implicitly performs *extract* command first
+
+@click.command(help='Graphically renders workflow model of script') # Implicitly performs *extract* and *model* commands first.
 @click.argument('sources', nargs=-1)  # Accepts zero or more arguments
 @click.pass_context
-def model(ctx, sources):
+def graph(ctx, sources):
     # Fetch properties
     properties = ctx.obj['properties']
     # Extract annotations
     annotations = extract_annotations(properties, sources)
     # Get name of top-level workflow
     workflow = "DEFAULT_WORKFLOW" if 'model.workflow' not in properties else properties['model.workflow']
+    layout = "TB" if 'graph.layout' not in properties else properties['graph.layout']
     # Create model
     create_model('\n'.join(annotations), workflow)
+    # Visualize the workflow
+    geist.report(inputfile='./src/yesworkflow/parse/graph.geist', isinputpath=True, suppressoutput=False, args={'layout': layout})
     return
